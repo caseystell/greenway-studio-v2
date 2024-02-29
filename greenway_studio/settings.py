5b7b962a6c62e8fc1cf7d0ca1a616f1ac22b875f
Casey Stell <cstell@pdnhie.com>
2024-02-29 09:49:17 -0700

    production-ready

--- greenway_studio/settings.py
+++ greenway_studio/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@dw2rkz)nmun+l*e2d7@a!$ve++3i#w-#gf9le)bj+wu3v)1_n'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
